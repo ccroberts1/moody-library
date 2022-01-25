e7b9a6089115adf8fb5c58d39e0e8d8917b5adc2
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-25 16:22:25 -0700

    started on schemas, typedefs and resolvers. Need to to do some more research on graphQL

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,25 @@
+const { Book } = require("../models");
+
+const resolvers = {
+  Query: {
+    books: async () => {
+      return Book.find();
+    },
+
+    book: async (parent, args) => {
+      return Book.findOne({ _id: bookId });
+    },
+  },
+
+  Mutation: {
+    addBook: async (parent, { title, author }) => {
+      return Book.create({ title, author });
+    },
+    removeBook: async (parent, { bookId }) => {
+      return Book.findOneAndDelete({ _id: bookId });
+    },
+    //TODO: Consider whether to add mutations for adding and removing tags
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+const { gql } = require("apollo-server-express");
+//TODO: Need to look up details on graphQL mutations and queries
+const typeDefs = gql`
+  type Book {
+    _id: ID
+    title: String!
+    author: String!
+    format: String
+    genre: String
+    length: Int
+    tags: [String]
+  }
+
+  type Query {
+    books: [Book]!
+    book(bookId: ID!): Book
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      format: String
+      genre: String
+      length: Int
+      tags: [String]
+    ): Book
+    removeBook(bookId: ID!): Book
+  }
+`;
+
+module.exports = typeDefs;
