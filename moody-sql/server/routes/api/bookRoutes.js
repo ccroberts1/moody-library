0318ffbd702fc5f7ac11d8095980d6999a7e1a6f
ccroberts1 <caitlin.coy.roberts@gmail.com>
2022-01-30 16:55:29 -0700

    added client directory, created tag routes and updated book routes to be try/catch format

--- moody-sql/server/routes/api/bookRoutes.js
+++ moody-sql/server/routes/api/bookRoutes.js
@@ -12,46 +12,52 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:bookId", async (req, res) => {
-  const bookData = await Book.findOne({
-    where: {
-      id: req.params.bookId,
-    },
-  }).catch((err) => {
-    res.json(err);
-  });
-  res.json(bookData);
-});
-
-router.put("/:bookId", async (req, res) => {
-  const updatedBook = await Book.update(
-    {
-      title: req.body.title,
-      author: req.body.author,
-      format: req.body.format,
-      genre: req.body.genre,
-      pages: req.body.pages,
-      tags: req.body.tags,
-    },
-    {
+  try {
+    const bookData = await Book.findOne({
       where: {
         id: req.params.bookId,
       },
-    }
-  ).catch((err) => {
+    });
+    res.json(bookData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:bookId", async (req, res) => {
+  try {
+    const updatedBook = await Book.update(
+      {
+        title: req.body.title,
+        author: req.body.author,
+        format: req.body.format,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        tags: req.body.tags,
+      },
+      {
+        where: {
+          id: req.params.bookId,
+        },
+      }
+    );
+    res.json(updatedBook);
+  } catch (err) {
     res.json(err);
-  });
-  res.json(updatedBook);
+  }
 });
 
 router.delete("/:bookId", async (req, res) => {
-  const deletedBook = await Book.destroy({
-    where: {
-      id: req.params.bookId,
-    },
-  }).catch((err) => {
+  try {
+    const deletedBook = await Book.destroy({
+      where: {
+        id: req.params.bookId,
+      },
+    });
+    res.json(deletedBook);
+  } catch (err) {
     res.json(err);
-  });
-  res.json(deletedBook);
+  }
 });
 
 router.post("/", async (req, res) => {
--- moody-sql/server/routes/api/tagRoutes.js
+++ moody-sql/server/routes/api/tagRoutes.js
@@ -1,5 +1,69 @@
 const router = require("express").Router();
 
-const Tag = require("../../models/Tag")
+const Tag = require("../../models/Tag");
+
+router.get("/", async (req, res) => {
+  try {
+    const tagData = await Tag.findAll();
+    res.json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:tagId", async (req, res) => {
+  try {
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.tagId,
+      },
+    });
+    res.json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:tagId", async (req, res) => {
+  try {
+    const updatedTag = await Tag.update(
+      {
+        name: req.body.name,
+      },
+      {
+        where: {
+          id: req.params.tagId,
+        },
+      }
+    );
+    res.json(updatedTag);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.delete("/:tagId", async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.tagId,
+      },
+    });
+    res.json(deletedTag);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newTag = await Tag.create({
+      name: req.body.name,
+    });
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
